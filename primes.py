c2d3acef22c0051ef5a9e5b934b66e06a645cb35
Aaditya Shivakumar <aaditya.shivakumar@kcl.ac.uk>
2023-09-29 20:15:02 +0100

    Extend primesv1 to raise value error if number of primes is less than 1

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from math import sqrt
+
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError(f"Number of primes should be a positive number.")
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+        num += 1
     return list
+
+def is_prime(num):
+    for i in range(2, int(sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
